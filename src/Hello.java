68f57058c48270b6d2554e52c668c5454978ac13
hwan7hwan <hwan7hwan@gmail.com>
2022-04-13 23:32:54 +0900

    Revert "3rd commit"
    This reverts commit 343dabaccb9e43e6998518953242becd7e43e5e5.

--- src/Hello.java
+++ src/Hello.java
@@ -1,26 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Iterator;
-
 public class Hello {
     public static void main(String[] args){
         String a = "What up git";
         String b = "Hello java!!";
 
-        List c =  new ArrayList();
-        c.add("list1");
-        c.add("list2");
-        c.add("list3");
-
         System.out.println(a);
         System.out.println(b);
-
-        Iterator getList = c.iterator();
-
-        while (getList.hasNext()){
-            String showMe =(String) getList.next();
-            System.out.println(showMe);
-        }
-
     }
 }
