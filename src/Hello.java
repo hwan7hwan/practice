92bbcdcc41d241605e1e20883afb0c521f90a496
hwan7hwan <hwan7hwan@gmail.com>
2022-04-13 23:15:02 +0900

    2nd commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
     public static void main(String[] args){
         String a = "What up git";
+        String b = "Hello java!!";
+
         System.out.println(a);
+        System.out.println(b);
     }
 }
